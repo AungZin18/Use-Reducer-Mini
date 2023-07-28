9a469cbb0c381e05a2576a7f0e2e0e566a03e941
AungZin18 <140605898+AungZin18@users.noreply.github.com>
2023-07-28 22:28:12 +0630

    fetch mini

--- src/App.jsx
+++ src/App.jsx
@@ -1,43 +1,52 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 
 function App() {
-  const [todos,setTodos]=useState([])
- useEffect(()=>{
-  fetchData()
- },[])
- const fetchData=async()=>{
-  const response =await fetch("https://jsonplaceholder.typicode.com/todos")
-  const data=await response.json()
-  setTodos(data)
-    }
-  return(
-  <section>
-     <table>
-    <thead>
-      <tr>
-        <th>ID</th>
-        <th>Title</th>
-        <th>Completed</th>
-      </tr>
-    </thead>
- 
-      {
-        todos.map(todo=>(
-          <tbody>
-            <tr>
-              <td>{todo.id}</td>
-              <td>{todo.title}</td>
-              {
-                todo.completed?(<td className="done">Done</td>):(<td className="none">None</td>)
-              }
-            </tr>
-          </tbody>
-        ))
-      }
+  const [id, setId] = useState("");
+  const [error, setError] = useState(false);
+  const [todo,setTodo]=useState(undefined)
 
-   </table>
-  </section>
-  )
+  const getData = async (e) => {
+    e.preventDefault();
+    if (id < 1) {
+      setError(true);
+      setId("");
+      return;
+
+    }
+    const response = await fetch(
+      `https://jsonplaceholder.typicode.com/todos/${id}`
+    );
+    const data = await response.json();
+    setError(false)
+setTodo(data)
+    setId("");
+  };
+
+  return (
+    <section>
+      <form onSubmit={getData}>
+        <input
+          type="number"
+          value={id}
+          onChange={(e) => {
+            setId(e.target.value);
+          }}
+        />
+        <button type="submit">Get Data</button>
+      </form>
+      <div>
+        {error && <h1>Please enter a valid id</h1>}
+        {todo && (
+          <div>
+            <h1>Id : {todo.id}</h1>
+            <h1>Title : {todo.title}</h1>
+            <h1>Userid : {todo.userId}</h1>
+            <h1>Completed : {todo.completed?<span>Done</span>:<span>None</span>}</h1>
+          </div>
+        )}
+        </div>
+    </section>
+  );
 }
 
 export default App;
